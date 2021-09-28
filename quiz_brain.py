cd4c57f74553173d4c2ce0c8c9a35d0a071152c6
myomyintaung <myomyintaung064@gmail.com>
2021-09-28 14:13:39 +0630

    Complete the Quiz App feature
    There is still bug that buttons cannot be disabled.

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,14 +1,16 @@
 import html
+from tkinter import StringVar
 
 
 class QuizBrain:
 
     def __init__(self, q_list):
+        self.question_list = q_list
         self.question_number = 0
         self.score = 0
-        self.question_list = q_list
         self.current_question = None
 
+    # to prevent index out of range error when there r no questions
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
@@ -16,17 +18,12 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        user_answer = input(
-            f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {q_text}"
 
-    def check_answer(self, user_answer):
+    def check_answer(self, user_answer) -> bool:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- ui.py
+++ ui.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from typing import Collection
+from quiz_brain import QuizBrain
+from tkinter import messagebox
 
 THEME_COLOR = "#375362"
 
 
 class QuizzInterface:
-    def __init__(self) -> None:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
         self.window = Tk()
         # self in the class means it is the attribute of this class OBJECT
-
+        self.quiz = quiz_brain
         self.window.title("My Quizzler")
         # TITLE of the APP Interface
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -22,20 +24,58 @@ class QuizzInterface:
         self.canvas_text = self.canvas.create_text(
             150,
             125,  # center of the window
-            text="Some Question text",
+            text="Some Question",
+            width=290,
             fill=THEME_COLOR,
             font=('Arial', 20, 'italic')
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(
+            image=true_image, highlightthickness=0, command=self.true_button)
         self.true_button.grid(row=2, column=0)
         # Using image as button
 
         false_image = PhotoImage(file="./images/false.png")
-        self.true_button = Button(image=false_image, highlightthickness=0)
+        self.true_button = Button(
+            image=false_image, highlightthickness=0, command=self.false_button)
         self.true_button.grid(row=2, column=1)
+
+        self.get_next_question()
 
         self.window.mainloop()
         # Keep updating the window
+
+    def get_next_question(self):
+        self.window.after(1000, self.canvas.config(bg="white"))
+
+        if self.quiz.still_has_questions():
+
+            q_text = self.quiz.next_question()
+            # If u forget () in calling function The App will display function name
+            self.canvas.itemconfig(self.canvas_text, text=q_text)
+            self.score_label.config(
+                text=f"Score : {self.quiz.score} / {self.quiz.question_number}")
+
+        else:
+            self.canvas.itemconfig(self.canvas_text, text="Out of questoins.")
+            self.false_button.config(state="disabled")
+            self.true_button.config(state="disabled")
+
+    def true_button(self):
+        # messagebox.showinfo(
+        #     title="Result", message=self.quiz.check_answer("true"))
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_button(self):
+        # messagebox.showinfo(
+        #     title="Result", message=self.quiz.check_answer("false"))
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
